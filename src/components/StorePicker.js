ea4e09b6962474eebc0337b3c3bc548d37ab9820
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-09-17 16:52:46 -0700

    button event binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,35 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
-    goToStore() {
-        console.log("going to the store");
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    myInput = React.createRef();
+
+    goToStore(event) {
+        //stop from from submitting
+        event.preventDefault();
+        // get the test from input
+       console.log(this.setState);
+    }
+
+    componentDidMount() {
+        console.log("mounted");
+        console.log(this);
     }
     render() {
         return (
             <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
                 <button ></button>
-                <input type="text" required placedholder="Store Name" defaultValue={getFunName()} />
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placedholder="Store Name"
+                    defaultValue={getFunName()} />
                 <button type="submit">Visit Store </button>
             </form>
         )
