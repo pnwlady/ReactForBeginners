8a4540566c7577134f65ec515b7ac34d00d060b2
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-09-17 16:01:09 -0700

    router added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
-// import StorePicker from './components/StorePicker';
-// import Header from './components/Inventory';
-// import Header from './components/Header';
-// import Order from './components/Order';
+import Router from './components/Router';
 import './css/style.css';
 
-render(<App></App>, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
