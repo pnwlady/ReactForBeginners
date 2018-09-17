f73e28ae696478a9eb83d60660b726433f8ab604
Heidi Laursen <heidi.laursen@terrastaffinggroup.com>
2018-09-17 16:38:37 -0700

    onsubmit firing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    goToStore() {
+        console.log("going to the store");
+    }
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placedholder="Store Name" value={getFunName()} />
+                <button ></button>
+                <input type="text" required placedholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit">Visit Store </button>
             </form>
         )
