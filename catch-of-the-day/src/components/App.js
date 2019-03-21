9e6f00110606af06e43fd20621e1a303ab210564
heidilaursen <heidilaursen@protonmail.com>
2019-03-21 11:43:45 -0700

    Inventory added

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from './Header';
+import Inventory from './Inventory';
+// import Header from './Header';
 
 class App extends React.Component {
   render() {
@@ -7,6 +9,7 @@ class App extends React.Component {
       <div className="catch-of-the-day">
         <div className="menu">
           <Header />
+          <Inventory />
         </div>
       </div>
     )
