9f73b1a74da2e003910fd0f618046007282f67c6
heidilaursen <heidilaursen@protonmail.com>
2019-03-21 14:16:41 -0700

    created fish form

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class Order extends React.Component {
   render() {
     return (
-      <div className="order">
-        order!!
+      <div className="Order">
+        <p>Orders here</p>
       </div>
-    )
+    );
   }
 }
 
