d0a7bcb92ec2d0c9604787ce46149a0938ae6711
heidilaursen <heidilaursen@protonmail.com>
2019-03-21 11:33:09 -0700

    building out app struction

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+      </div>
+    )
+  }
+}
+export default App;
